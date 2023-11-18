9a4fbfecaf2b7389fd0e5f3e34a8ef7ec3ef87b8
MwanikiN <neemaclare@gmail.com>
2023-11-18 14:14:16 +0300

    plus_one search_position word_length

---
+++ search_insert_position.py
@@ -0,0 +1,51 @@
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+# If not, return the index where it would be if it were inserted in order.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+# Example 1:
+
+# Input: nums = [1,3,5,6], target = 5
+# Output: 2
+# Example 2:
+
+# Input: nums = [1,3,5,6], target = 2
+# Output: 1
+# Example 3:
+
+# Input: nums = [1,3,5,6], target = 7
+# Output: 4
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 104
+# -104 <= nums[i] <= 104
+# nums contains distinct values sorted in ascending order.
+# -104 <= target <= 104
+def search_insert(nums, target):
+    for i, n in enumerate(nums):
+        if n >= target:
+            return i
+    return len(nums)
+
+print(search_insert([1,3,5,6], 7))  
+print(search_insert([1,3,5,6], 5))
+print(search_insert([1,3,5,6], 2))
+
+# alternative approach
+def search_insert(nums, target):
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return left
+
+print(search_insert([1,3,5,6], 7))
+print(search_insert([1,3,5,6], 5))
+print(search_insert([1,3,5,6], 2))
